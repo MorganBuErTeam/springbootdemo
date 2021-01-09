073fa8ec0b20b248077c6f4c14b5e70ec8521423
MorganBuErTeam <361134300@qq.com>
2021-01-09 20:45:45 +0800

    a

--- src/main/java/com/test/demo/controller/TestMethodController.java
+++ src/main/java/com/test/demo/controller/TestMethodController.java
@@ -36,6 +36,7 @@ public class TestMethodController {
 
     @PostMapping(value = "/sdfsdf")
     public UpKeepInfo sjsflsadjfl(@RequestBody Task task) {
+        System.out.println("a");
         return new UpKeepInfo();
     }
 
