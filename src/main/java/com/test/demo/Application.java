2e0cb8b84f764d99fc781f03f09d3f48fa3bc122
MorganBuErTeam <361134300@qq.com>
2021-01-10 18:19:16 +0800

    a

--- src/main/java/com/test/demo/Application.java
+++ src/main/java/com/test/demo/Application.java
@@ -1,18 +1,10 @@
 
 package com.test.demo;
 
-import org.apache.catalina.Context;
-import org.apache.catalina.connector.Connector;
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.jms.annotation.EnableJms;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 /**
@@ -20,6 +12,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  */
 @SpringBootApplication
 @ComponentScan(basePackages={"com.test.demo"})
+@NacosPropertySource(dataId = "nacostest", autoRefreshed = true)
 public class Application {
     //@EnableScheduling 定时任务注解
 //    @EnableJms    //启动消息队列
@@ -27,35 +20,7 @@ public class Application {
         new SpringApplicationBuilder(Application.class).run(args);
     }
 
-    /**
-     * it's for set http url auto change to https
-     */
-//    @Bean
-//    public EmbeddedServletContainerFactory servletContainer(){
-//        TomcatEmbeddedServletContainerFactory tomcat=new TomcatEmbeddedServletContainerFactory(){
-//            @Override
-//            protected void postProcessContext(Context context) {
-//                SecurityConstraint securityConstraint=new SecurityConstraint();
-//                securityConstraint.setUserConstraint("CONFIDENTIAL");//confidential
-//                SecurityCollection collection=new SecurityCollection();
-//                collection.addPattern("/*");
-//                securityConstraint.addCollection(collection);
-//                context.addConstraint(securityConstraint);
-//            }
-//        };
-//        tomcat.addAdditionalTomcatConnectors(httpConnector());
-//        return tomcat;
-//    }
 
-//    @Bean
-//    public Connector httpConnector(){
-//        Connector connector=new Connector("org.apache.coyote.http11.Http11NioProtocol");
-//        connector.setScheme("http");
-//        connector.setPort(8082);
-//        connector.setSecure(false);
-//        connector.setRedirectPort(8443);
-//        return connector;
-//    }
 
 
 }
